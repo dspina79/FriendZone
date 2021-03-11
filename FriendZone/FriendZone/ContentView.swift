e3179d6f0fada754ceff1cc5cbdb35ff3807985d
Dave Spina <dspina79@gmail.com>
2021-03-10 20:15:39 -0500

    Project initialization

---
+++ FriendZone/FriendZone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FriendZone
+//
+//  Created by Dave Spina on 3/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FriendZone/FriendZone/FriendZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendZoneApp.swift
+//  FriendZone
+//
+//  Created by Dave Spina on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct FriendZoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
